b871cf7ce321a69ae9a956e0bd34a484c17acd3d
Sigafoos <dan.j.conley@gmail.com>
2013-02-11 12:58:13 -0500

    Added 'listen'/'hide' text to the link to make it more obvious you can click it.

--- now-playing.js
+++ now-playing.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
 		$('#nowplaying a').click(function(){
 			if (!$('#video').is(':visible')) {
 			$('#video').slideDown('fast');
-			$('#arrow').html('<i class="icon-angle-up icon-large"></i>');
+			$('#arrow').html('hide <i class="icon-angle-up icon-large"></i>');
 			} else {
 			$('#video').slideUp('fast');
-			$('#arrow').html('<i class="icon-angle-down icon-large"></i>');
+			$('#arrow').html('listen <i class="icon-angle-down icon-large"></i>');
 			}
 			});
 		});
